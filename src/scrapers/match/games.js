605523432ee4b6638cfda5d55ffe5f691b4ccec3
Erik Wood <102174722+erikhwoodff@users.noreply.github.com>
2024-02-19 10:46:55 -0500

    Update games.js

--- src/scrapers/match/games.js
+++ src/scrapers/match/games.js
@@ -6,34 +6,30 @@ const cheerio = require('cheerio');
 
 const fetchGamesMatch = async (matchId) => {
     return new Promise(async (resolve, reject) => {
-        // Fetch the page
         axios.get(`https://www.vlr.gg/${matchId}`)
             .then((response) => {
-                // Parse the page
                 let $ = cheerio.load(response.data);
                 const Match = {};
 
-                // Extract match_id from the URL
                 Match.match_id = matchId;
-
-                // Extract game_ids, map numbers, and map names, ignoring the "all" entries
                 Match.games = [];
+
                 $(".vm-stats-gamesnav-item.js-map-switch[data-game-id]").each((i, element) => {
                     const game_id = $(element).attr("data-game-id");
                     const mapNumber = $(element).data("href").split('=')[1];
+                    
+                    // Skip if the game_id or mapNumber is 'all'
+                    if (game_id === "all" || mapNumber === "all") return;
 
-                    // Assuming the map name is the next text node after the span containing the map number
-                    const mapName = $(element).next('div').contents().filter(function() {
-                        return this.type === 'text';
-                    }).text().trim();
+                    // The map name seems to be the text node immediately following the map number span
+                    // This will get the text node directly after the span containing the map number
+                    let mapName = $(element).next('div').find('span').first().next().text().trim();
 
-                    if (game_id !== "all" && mapNumber !== "all" && mapName) {
-                        Match.games.push({
-                            game_id: game_id,
-                            map_number: mapNumber,
-                            map_name: mapName // Add map name to our game object
-                        });
-                    }
+                    Match.games.push({
+                        game_id: game_id,
+                        map_number: mapNumber,
+                        map_name: mapName
+                    });
                 });
 
                 resolve(Match);
@@ -45,4 +41,3 @@ const fetchGamesMatch = async (matchId) => {
 }
 
 module.exports = { fetchGamesMatch };
-
