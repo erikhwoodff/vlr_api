2291f1b9ad203f79b3643e763d12db188ab82e8c
Erik Wood <102174722+erikhwoodff@users.noreply.github.com>
2024-02-19 21:24:31 -0500

    Update stats.js
    adding first iteration/attempt at players stats

--- src/scrapers/match/stats.js
+++ src/scrapers/match/stats.js
@@ -1,4 +1,3 @@
-// External Libs
 const axios = require('axios');
 const cheerio = require('cheerio');
 
@@ -26,54 +25,86 @@ const fetchStatsMatch = async (matchId) => {
                 const eventDateElement = $(".moment-tz-convert").first();
                 Match.event_utc_ts = eventDateElement.length ? eventDateElement.data("utc-ts") : null;
 
-                // Logic to extract games data
-                Match.games = [];
+                // Initialize the players array
+                Match.players = [];
 
-                $(".vm-stats-gamesnav-item.js-map-switch[data-game-id]").each((i, element) => {
-                    const game_id = $(element).attr("data-game-id");
-                    const href = $(element).data("href");
-                    const mapNumber = href ? href.split('=')[1] : null;
+                // Iterate over each player row in the table
+                $(".wf-table-inset.mod-overview tr").each((index, element) => {
+                    if ($(element).find(".mod-player a div:nth-child(1)").text().trim() === "") return;
 
-                    // Skip if the game_id or mapNumber is 'all'
-                    if (game_id === "all" || mapNumber === "all") return;
+                    const Player = {};
+                    Player.name = $(element).find(".mod-player a div:nth-child(1)").text().trim();
+                    Player.team = $(element).find(".mod-player a div:nth-child(2)").text().trim();
+                    Player.link = `https://www.vlr.gg${$(element).find(".mod-player a").attr("href")}`;
+                    Player.statsadvanced = {};
+                    Player.stats = {};
 
-                    // Find the map name using the game_id to select the right .vm-stats-game element
-                    let mapNameSelector = `.vm-stats-container .vm-stats-game[data-game-id='${game_id}'] .map`;
-                    let mapName = $(mapNameSelector).text().trim().split("\t")[0].trim();
+                    const playerStats = $(element).find(".mod-stat");
+                    playerStats.each((i, element) => {
+                        const ct = $(element).find(".mod-ct").text().trim();
+                        const t = $(element).find(".mod-t").text().trim();
+                        const ot = $(element).find(".mod-ot").text().trim();
+                        const both = $(element).find(".mod-both").text().trim();
+                        const data = {
+                            ct: ct,
+                            t: t,
+                            ot: ot
+                        };
+                        switch (i) {
+                            case 0:
+                                Player.statsadvanced.kdr = data;
+                                Player.stats.kdr = both;
+                                break;
+                            case 1:
+                                Player.statsadvanced.acs = data;
+                                Player.stats.acs = both;
+                                break;
+                            case 2:
+                                Player.statsadvanced.k = data;
+                                Player.stats.k = both;
+                                break;
+                            case 3:
+                                Player.statsadvanced.d = data;
+                                Player.stats.d = both;
+                                break;
+                            case 4:
+                                Player.statsadvanced.a = data;
+                                Player.stats.a = both;
+                                break;
+                            case 5:
+                                Player.statsadvanced.kdb = data;
+                                Player.stats.kdb = both;
+                                break;
+                            case 6:
+                                Player.statsadvanced.kast = data;
+                                Player.stats.kast = both;
+                                break;
+                            case 7:
+                                Player.statsadvanced.adr = data;
+                                Player.stats.adr = both;
+                                break;
+                            case 8:
+                                Player.statsadvanced.hs = data;
+                                Player.stats.hs = both;
+                                break;
+                            case 9:
+                                Player.statsadvanced.fk = data;
+                                Player.stats.fk = both;
+                                break;
+                            case 10:
+                                Player.statsadvanced.fd = data;
+                                Player.stats.fd = both;
+                                break;
+                            case 11:
+                                Player.statsadvanced.fkdb = data;
+                                Player.stats.fkdb = both;
+                                break;
+                            default:
+                                break;
+                        }
+                    });
 
-                    // If the map name is empty, skip this game
-                    if (!mapName) return;
-
-                    // Extracting team names and scores
-                    let teamData = $(`.vm-stats-container .vm-stats-game[data-game-id='${game_id}']`);
-                    let teamNames = teamData.find(".team-name").map((idx, el) => $(el).text().trim()).get();
-                    let mod_cts = teamData.find(".mod-ct").map((idx, el) => $(el).text().trim()).get();
-                    let mod_ts = teamData.find(".mod-t").map((idx, el) => $(el).text().trim()).get();
-                    let mod_ots = teamData.find(".mod-ot").map((idx, el) => $(el).text().trim()).get();
-
-                    // Construct the game data object
-                    let gameData = {
-                        game_id: game_id,
-                        map_number: mapNumber,
-                        map_name: mapName,
-                        teams: [
-                            {
-                                name: teamNames[0],
-                                mod_ct: mod_cts[0],
-                                mod_t: mod_ts[0],
-                                mod_ot: mod_ots[0] || "0"
-                            },
-                            {
-                                name: teamNames[1],
-                                mod_ct: mod_cts[1],
-                                mod_t: mod_ts[1],
-                                mod_ot: mod_ots[1] || "0"
-                            }
-                        ]
-                    };
-
-                    // Add the game data to the Match.games array
-                    Match.games.push(gameData);
+                    Match.players.push(Player);
                 });
 
                 resolve(Match);
