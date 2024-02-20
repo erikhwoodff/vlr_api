10396bee50b07bbc53c60c97e034db8bac1a6922
Erik Wood <102174722+erikhwoodff@users.noreply.github.com>
2024-02-20 00:21:10 -0500

    Update stats.js
    back to this

--- src/scrapers/match/stats.js
+++ src/scrapers/match/stats.js
@@ -2,50 +2,41 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 function cleanString(str) {
-    return str.replace(/[\n\t\r]+/g, ' ').replace(/\s\s+/g, ' ').trim();
+    return str.replace(/[\n\t]/g, '').trim().replace(/\s\s+/g, ' ');
 }
 
 const fetchStatsMatch = async (matchId) => {
-    try {
-        const response = await axios.get(`https://www.vlr.gg/${matchId}`);
-        let $ = cheerio.load(response.data);
-        const Match = {};
-        Match.match_id = matchId;
-
+    return new Promise((resolve, reject) => {
+        axios.get(`https://www.vlr.gg/${matchId}`)
+            .then((response) => {
+                let $ = cheerio.load(response.data);
+                const Match = {};
+                Match.match_id = matchId;
                 // Extract the main Event name and clean up whitespace and newline characters
                 let rawMainEvent = $(".match-header-super div[style*='font-weight: 700;']").first().text();
                 Match.event_name = rawMainEvent.replace(/\s+/g, ' ').trim(); 
-
                 // Extract the Sub Event
                 let rawSubEvent = $(".match-header-super .match-header-event-series").first().text();
                 Match.sub_event = rawSubEvent.replace(/\s+/g, ' ').trim(); 
-
                 // Extract the event ID from the href attribute
                 const eventHref = $("div.match-header-super a.match-header-event").attr("href");
                 Match.event_id = eventHref ? eventHref.split('/')[2] : null;
-
                 // Extract the timestamp from the data-utc-ts attribute
                 const eventDateElement = $(".moment-tz-convert").first();
                 Match.event_utc_ts = eventDateElement.length ? eventDateElement.data("utc-ts") : null;
-
                 // Logic to extract games data
                 Match.games = [];
-        
                 $(".vm-stats-gamesnav-item.js-map-switch[data-game-id]").each((gameIndex, gameElement) => {
                     const game_id = $(gameElement).attr("data-game-id");
                     const href = $(gameElement).data("href");
                     const mapNumber = href ? href.split('=')[1] : null;
-
                     // Skip if the game_id or mapNumber is 'all'
                     if (game_id === "all" || mapNumber === "all") return;
-
                     // Find the map name using the game_id to select the right .vm-stats-game element
                     let mapNameSelector = `.vm-stats-container .vm-stats-game[data-game-id='${game_id}'] .map`;
                     let mapName = $(mapNameSelector).text().trim().split("\t")[0].trim();
-
                     // If the map name is empty, skip this game
                     if (!mapName) return;
-
                     // Initialize the game object
                     const game = {
                         game_id: game_id,
@@ -53,36 +44,52 @@ const fetchStatsMatch = async (matchId) => {
                         map_name: mapName,
                         players: []
                     };
-
                     // Iterate over each player row in the table for this game
                     $(`.vm-stats-container .vm-stats-game[data-game-id='${game_id}'] .wf-table-inset.mod-overview tr`).each((playerIndex, playerElement) => {
-                        const player = {};
+                        if ($(playerElement).find(".mod-player").text().trim() === "") return;
+
+                        const Player = {};
+                        Player.name = $(playerElement).find(".mod-player").text().trim(); // Assuming the player's name is within .mod-player
+                                            Player.team = $(playerElement).find(".mod-player a div:nth-child(2)").text().trim();
                         const playerNameWithExtra = $(playerElement).find(".mod-player").text();
-                        player.name = cleanString(playerNameWithExtra);
-                        
+                        const playerName = cleanString(playerNameWithExtra); // Cleaned player name
+
                         const playerLink = $(playerElement).find(".mod-player a").attr("href");
                         Player.player_id = playerLink ? playerLink.split('/')[2] : null;
                         Player.stats = {};
                     
                         // Extract each stat for the player
-                        player.stats = {};
-
-                $(playerElement).find("td[class*='mod-stat']").each((statIndex, statElement) => {
-                    const statName = $(statElement).attr('class').split(' ').find(cls => cls.includes('mod-vlr')).split('-').pop();
-                    let statValueWithExtra = $(statElement).find('.stats-sq').text();
-                    player.stats[statName] = cleanString(statValueWithExtra);
+                        $(playerElement).find("td[class*='mod-stat']").each((statIndex, statElement) => {
+                            // Safely get the class name containing the stat
+                            const classList = $(statElement).attr('class').split(' ');
+                            const statClass = classList.find(cls => cls.includes('mod-vlr'));
+                            // If a statClass isn't found, or doesn't contain a '-', skip this element
+                            if (!statClass || !statClass.includes('-')) return;
+                        
+                            const statNameParts = statClass.split('-');
+                            // Check if statNameParts has the expected length
+                            if (statNameParts.length < 3) return;
+                        
+                            const statName = statNameParts[2]; // This is the stat name
+                            let statValue = $(statElement).find('.stats-sq').text();
+                        
+                            // Clean up the statValue by removing newlines, tabs, and multiple spaces
+                            statValue = statValue.replace(/[\n\t]+/g, '').trim(); // Remove newlines and tabs
+                            statValue = statValue.replace(/\s\s+/g, ' '); // Replace multiple spaces with a single space
+                        
+                            Player.stats[statName] = statValue;
+                        });
+                    
+                        game.players.push(Player);
+                    });
+                    // Add the game object to the Match.games array
+                    Match.games.push(game);
                 });
-
-                game.players.push(player);
+                resolve(Match);
+            })
+            .catch((err) => {
+                reject(err);
             });
-
-            Match.games.push(game);
-        });
-
-        return Match; // Resolve the promise with the match data
-    } catch (err) {
-        throw err; // Reject the promise with an error
-    }
+    });
 }
-
 module.exports = { fetchStatsMatch };
