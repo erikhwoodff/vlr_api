cac90a41c53895a2c389b25bddc45256db9e6790
Erik Wood <102174722+erikhwoodff@users.noreply.github.com>
2024-03-02 20:09:59 -0500

    Updating schedule.js

--- src/scrapers/match/schedule.js
+++ src/scrapers/match/schedule.js
@@ -3,27 +3,20 @@ const cheerio = require('cheerio');
 
 const fetchScheduleIds = async () => {
     try {
-        // Fetch the page
         const response = await axios.get(`https://www.vlr.gg/matches`);
         const $ = cheerio.load(response.data);
         const matchIds = [];
         
-        // Target only <a> elements that contain 'matches' in the href attribute
         const matchLinks = $("a[href*='/matches/']");
-
+        
         matchLinks.each((i, element) => {
             const href = $(element).attr("href");
-            console.log(`Processing href: ${href}`);
-            
-            // Extract the match ID, which is the number after 'matches/'
-            const matchId = href.split('/')[2]; // Assuming the number after 'matches/' is the ID
-            if (matchId && /^\d+$/.test(matchId)) { // Check if extracted part is a number
+            const matchId = href.split('/')[2];
+            if (matchId && /^\d+$/.test(matchId)) {
                 matchIds.push(matchId);
-                console.log(`Extracted matchId: ${matchId}`);
             }
         });
 
-        console.log('Match IDs fetched:', matchIds);
         return matchIds;
     } catch (err) {
         console.error(`Error fetching match IDs: ${err}`);
@@ -31,9 +24,4 @@ const fetchScheduleIds = async () => {
     }
 };
 
-// Usage
-fetchScheduleIds().then(matchIds => {
-    console.log(matchIds);
-}).catch(err => {
-    console.error(err);
-});
+module.exports = fetchScheduleIds;
